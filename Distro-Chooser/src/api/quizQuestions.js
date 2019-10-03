d82aba2ae0090c9cd788129cc2bb9307d2968cef
Anuj kapoor <kapoor.anuj0@gmail.com>
2019-10-03 11:51:53 +0530

    React Based Frontend

---
+++ Distro-Chooser/src/api/quizQuestions.js
@@ -0,0 +1,38 @@
+var quizQuestions = [
+  {
+      question: "Software: Use case?",
+      answers: [
+          {
+              type: "Ubuntu",
+              content: "Daily"
+          },
+          {
+              type: "Kali",
+              content: "anonymous web browsing."
+          },
+          {
+              type: "Manjaro",
+              content: "For Development"
+          }
+      ]
+  },
+  {
+      question: "How would I rate my knowledge using my computer?",
+      answers: [
+          {
+              type: "Ubuntu",
+              content: "Beginner"
+          },
+          {
+              type: "Kali",
+              content: "Intermediate"
+          },
+          {
+              type: "Manjaro",
+              content: "Pro"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
